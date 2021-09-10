83b0ee35fa356a31980570165556bae4e71141cf
Kevin Rogers <kevinarogers1@gmail.com>
2021-09-09 21:16:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2, num2 = 31;
+let multiply = num1 * num2
+    multiply;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10); 
+    
+let num3 = 20 , num4 = 5;
+
+let mod = num3 / num4
+    mod; 
+
+let max = Math.max(1,2,3,20)
+    max;
